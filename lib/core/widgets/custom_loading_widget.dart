47bc68892f559c000aa608eb88bf82e5c45fa6fa
hassan553 <hassanmarzouk042@gmail.com>
2024-01-05 00:21:17 -0800

    custom loading widget

--- lib/core/widgets/custom_loading_widget.dart
+++ lib/core/widgets/custom_loading_widget.dart
@@ -1,15 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../rescourcs/app_colors.dart';
 
 class CustomLoadingWidget extends StatelessWidget {
   final Color? color;
-  const CustomLoadingWidget({super.key,this.color=AppColors.primaryColor});
+  const CustomLoadingWidget({super.key, this.color = AppColors.primaryColor});
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: CircularProgressIndicator(color: color),
+    final plateForm = Theme.of(context).platform;
+    return Center(
+      child: plateForm == TargetPlatform.iOS
+          ? CupertinoActivityIndicator(color: color)
+          : CircularProgressIndicator(color: color),
     );
   }
 }
